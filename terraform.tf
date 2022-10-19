edb10318ec96488da8b9bab35c807bd68c8b6de7
Hai Nam <95199255+namhh5820@users.noreply.github.com>
2022-10-20 01:12:44 +0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 
   cloud {
-    organization = "organization-name"
+    organization = "hainamkhtn"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "learn-terraform-cloud-vcs"
     }
   }
 
